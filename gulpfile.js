ef83f6ac341fb24e722c5164b57f0626d2866411
Jamin Ewell <jamin.ewell@yahoo.com>
2020-10-22 23:51:29 -0700

    final changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,14 @@
-var { src, dest, watch } = require('gulp');
+var {src, dest, watch} = require('gulp');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-function css() {
+function css () {
     return src('src/*.scss')
         .pipe(sass())
         .pipe(dest('dist/css'))
 };
 
-//exports.default = function() {
-//    watch('src/*.scss', css);
-//   watch('src/modules/*.scss', css);
-//};
-
-//exports.default = css;
-
-exports.default = function() {
+exports.default = function () {
     watch('src/*.scss', css);
     watch('src/modules/*.scss', css);
 };
